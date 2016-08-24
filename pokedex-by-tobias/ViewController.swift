ed6ff83d85993ebb2d8bdca47054782fe476dbd3
Tobias Gozzi <tobias.gozzi@me.com>
2016-08-24 22:13:44 +0200

    Initial Commit

---
+++ pokedex-by-tobias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-tobias
+//
+//  Created by Tobias Gozzi on 24.08.16.
+//  Copyright © 2016 Tobias Gozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
